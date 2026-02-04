1140b8527a5ae84f7eecd2cd949f0b3f631e26ac
santhosh <santhoshannadurai6256@gmail.com>
2026-02-04 20:28:16 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -5,8 +5,10 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
+
         response.getWriter().println("Hello from WildFly CI/CD Pipeline!");
     }
 }
