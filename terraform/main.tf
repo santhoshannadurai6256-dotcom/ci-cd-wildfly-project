a374f4b70132e2835616855da3d2bde5b4a38b7b
santhosh <santhoshannadurai6256@gmail.com>
2026-02-04 12:23:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-resource "aws_instance" "devops" {
-  ami           = var.ami
-  instance_type = "c7i-flex.large"
-  key_name      = var.key_name
-
-  tags = {
-    Name = "devops-server"
-  }
-}
-
 resource "aws_instance" "wildfly" {
   count         = 2
   ami           = var.ami
